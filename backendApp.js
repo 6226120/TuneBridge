8eb8fa9771ade4995a36c93402b12cee73ff7f32
Karan Arora <ar0ra.karan7890@gmail.com>
2025-05-07 13:02:05 -0600

    Add global styles and layout for the application
    - Created globals.css for defining CSS variables and styles
    - Implemented layout.js to set up the root layout with custom fonts
    - Developed page.js to structure the home page with responsive design and links

--- backendApp.js
+++ backendApp.js
@@ -1,39 +1,62 @@
-import { configDotenv } from 'dotenv'
-configDotenv.config 
-const express = require('express')
-const request = require('request')
-const QueryString = require('querystring')
-const cors = require('cors')
+import dotenv from 'dotenv';
+dotenv.config(); 
+import express from 'express'
+import newBuffer  from 'buffer'
+import request from 'request'
+import  fetch  from 'node-fetch'
+import * as querystring from 'querystring'
+import cors from 'cors'
 const app = express()
 const port = 3000
 
-Client_id = 'CLIENT_ID'
-CLient_secret = 'CLIENT_SECRET'
+let Client_id = process.env.CLIENT_ID
+let Client_secret = process.env.CLIENT_SECRET
 const redirect_uri = 'http://localhost:3000/callback'
 
+app.get('/', (req, res) => {
+    res.send(`
+        <html>
+          <head><title>TuneBridge</title></head>
+          <body>
+            <h1>Welcome to TuneBridge</h1>
+            <a href="/login">
+              <button>Sign in with Spotify</button>
+            </a>
+          </body>
+        </html>
+      `);
+  });
 app.get('/login', (req, res) => {
-    var state = generateRanomString(32);
-    var scope = 'user-read-private user playlist-read-private playlist-modify-private'
-    
-    res.redirect('https://accounts.spotyify.com/authorize?' + QueryString.stringify({
-        reponse_type: 'code',
+    var state = generateRandomString(32);
+    const scope = 'user-read-private user-read-email playlist-read-private playlist-modify-private';
+
+
+    const params = querystring.stringify({
+        response_type: 'code',
         client_id: Client_id,
         scope: scope,
         redirect_uri: redirect_uri,
         state: state
-    }))
+      });
+      
+      res.redirect('https://accounts.spotify.com/authorize?' + params);
+      console.log("Working So far 1");
+    })
 
-})
 app.get('/callback', (req, res) => {
     var code = req.query.code || null;
     var state = req.query.state || null;
+    
 
     if(state === null){
-        res.redirect('/#' + quertstring.stringify({
+        console.log("Working So far 3.1")
+        res.redirect('/#' + querystring.stringify({
             error: 'state_mismatch'
         }))
     } else {
+        
         var authOptions = {
+
             url: 'https://accounts.spotify.com/api/token',
             form: {
                 code: code,
@@ -42,7 +65,7 @@ app.get('/callback', (req, res) => {
             },
             headers: {
                 'content-type': 'applciation/x-www-form-urlencoded',
-                'Authorization' : 'Basic ' +(newBuffer.from(client_id + ':' + CLient_secret).toString('base64'))
+                'Authorization' : 'Basic ' +(Buffer.from(Client_id + ':' + Client_secret).toString('base64'))
             },
             json: true
         };
@@ -51,8 +74,32 @@ app.get('/callback', (req, res) => {
             if(!error && response.statusCode === 200){
                 var access_token = body.access_token,
                 refresh_token = body.refresh_token
+
+                var options = {
+                    url: 'https://api.spotify.com/v1/me',
+                    headers: { 'Authorization': 'Bearer ' + access_token },
+                    json: true
+                  };
+          
+                  // use the access token to access the Spotify Web API
+                  request.get(options, function(error, response, body) {
+                    console.log(body);
+                  });
+          
+                  // we can also pass the token to the browser to make requests from there
+                  res.redirect('/#' +
+                    querystring.stringify({
+                      access_token: access_token,
+                      refresh_token: refresh_token
+                    }));
+                } else {
+                  res.redirect('/#' +
+                    querystring.stringify({
+                      error: 'invalid_token'
+                    }));
             }
         });
+
 
     }
 });
@@ -63,7 +110,7 @@ app.get('/refresh_token', (req, res)=>{
         url: 'https://accounts.spotify.com/api/token',
         headers: {
             'content-type': 'application/x-www-form-urlencoded',
-            'Authoirzation': 'Basic ' + (new Bugffer.from(client_id + ':' + client_secret).toString('base64'))
+            'Authoirzation': 'Basic ' + (Buffer.from(Client_id + ':' + Client_secret).toString('base64'))
         },
         form: {
             grant_type: 'refresh_token',
@@ -84,7 +131,7 @@ app.get('/refresh_token', (req, res)=>{
         }
     });
 });
-app.get('/users/:user_id/playlists', (req, res) => {
+const getPlaylist = function (req, res) {
 
     user_id = req.params.user_id;
     var authOptions = {
@@ -107,4 +154,21 @@ app.get('/users/:user_id/playlists', (req, res) => {
         }
         return response.json();
     })
-});
+};
+
+function generateRandomString(length) {
+    let result = '';
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const charactersLength = characters.length;
+    
+    for (let i = 0; i < length; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+  
+    return result;
+  }
+
+
+app.listen(port, () => {
+    console.log(`✅ Server is running at http://localhost:${port}`);
+  });
